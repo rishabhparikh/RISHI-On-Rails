3e488805408aeb1156940f64c2edf7f41de3591c
Rishabh Parikh <rishabh@berkeley.edu>
2016-02-09 12:23:07 -0800

    Initial rails frame

---
+++ rishi-on-rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RishiOnRails::Application.config.secret_key_base = 'a031abe121e1b09fbc599958aa45916f4b4be0cda3940294ba16ecac08cf2e7d255f7636df41ec7b470781ef84667d12902abdc738d325e5bda8b58206267359'
---
+++ rishi-on-rails/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RishiOnRails::Application.config.session_store :cookie_store, key: '_rishi-on-rails_session'
